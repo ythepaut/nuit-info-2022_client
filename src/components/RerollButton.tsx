f9255996c4616a1f6bcda15d216388ddfdd6da4c
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-12-02 01:40:08 +0100

    feat(N2I-23): add RerrolButton component

---
+++ src/components/RerollButton.tsx
@@ -0,0 +1,7 @@
+export default function RerollButton() {
+    return (
+        <div className="absolute bottom-3 left-3">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Repiocher</button>
+        </div>
+    );
+}
