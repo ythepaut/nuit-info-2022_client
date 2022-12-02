7a25860a24f3deff9cffdac23006467d5b92a487
RealDragonMA <realdragonma@gmail.com>
2022-12-02 05:44:39 +0100

    fix(N23-32): new style #2

--- src/components/cards/SituationCard.tsx
+++ src/components/cards/SituationCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function SituationCard(props: Props): JSX.Element {
     return (
-        <div className="card shadow-lg bg-gray-600 flex">
+        <div className="card shadow-lg bg-pink-900 border-4 border-white flex">
             <p className={"font-bold text-xl text-gray-200"}>{props.card ? props.card.message : ""}</p>
         </div>
     );
